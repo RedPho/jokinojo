fee7383008b40429d4ca2dc4e10d95adcf7cfde6
Kutay Balta <kutaybalta@yahoo.com>
2024-11-28 20:04:50 +0300

    join room yazdim henuz test etmedim ve proto dosyasinda responsea error bölümü ekledim

--- python/chat/server.py
+++ python/chat/server.py
@@ -8,6 +8,7 @@ users = []
 rooms = []
 
 def handle_client(user: User):
+
     while True:
         try:
             message = user.socket.recv(1024)
@@ -16,7 +17,7 @@ def handle_client(user: User):
                 break
 
             print(f"Received from {user.user_name}: {message}")
-            response = handle_request(message)
+            response = handle_request(message, user)
             user.socket.send(response.SerializeToString())
         except Exception as e:
             print(f"Error for {user.user_name}: {e}")
@@ -24,22 +25,55 @@ def handle_client(user: User):
 
 
 
-def handle_request(raw_data):
+def handle_request(raw_data, user):
     request = pb.RequestData()
     request.ParseFromString(raw_data)
 
     request_type = request.dataType
     print(f"{request_type} received")
     if request_type == pb.RequestData.CREATE_ROOM:
-        room = Room()
-        rooms.append(room)
-        print(f"Room created with id {room.room_id}")
-        response = pb.ResponseData()
-        response.dataType = pb.ResponseData.CREATE_ROOM
-        response.roomId = room.room_id
-
+        response = create_room(request, user)
+        return response
+    elif request_type == pb.RequestData.JOIN_ROOM:
+        response = join_room(request, user)
         return response
 
+def create_room(request, user):
+    user.user_name = request.username
+    room = Room()
+    room.add_user(user)
+    rooms.append(room)
+    print(f"Room created with id {room.room_id}")
+    response = pb.ResponseData()
+    response.dataType = pb.ResponseData.CREATE_ROOM
+    response.roomId = room.room_id
+    return response
+
+def join_room(request, user):
+    user.user_name = request.username
+    room_id = request.roomId
+    is_room_exist = False
+    for room in rooms:
+        if room.room_id == room_id:
+            is_room_exist = True
+            room.add_user(user)
+            if room.ready:
+                room.ready = False
+                pause_all_users(room)
+            #ready degiskeni dogruysa false yapip
+            #tüm kullanicilara pause atsin
+
+    if is_room_exist:
+        response = pb.ResponseData()
+        response.dataType = pb.ResponseData.JOIN_ROOM
+        response.usernames.extend([user.user_name for user in room.users])
+        response.videoName = room.video_name
+        return response
+    else:
+        response = pb.ResponseData()
+        response.dataType = pb.ResponseData.ERROR
+        response.errorMessage = "This room doesnt exist"
+        return response
 
 
 # Server setup
