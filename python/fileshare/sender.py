99d460de3558b7a9bae6b8770845ff0bdf6f0270
Kutay Balta <kutaybalta@yahoo.com>
2024-12-18 14:37:34 +0300

    dosya gönderimi ilk defa calisiyor

--- python/fileshare/sender.py
+++ python/fileshare/sender.py
@@ -2,7 +2,8 @@
 import os
 import hashlib
 import socket
-from file_pb2 import FileShare
+import file_pb2 as pb
+import struct
 
 CHUNK_SIZE = 1024 * 1024  # 1 MB
 
@@ -15,6 +16,13 @@ class FileSender:
         self.file_hash = self.calculate_hash()
         self.host = host
         self.port = port
+
+    def send_message(self, s, message):
+        serialized_message = message.SerializeToString()
+        message_length = len(serialized_message)
+        # Pack the length into 4 bytes using network byte order
+        s.sendall(struct.pack('!I', message_length))
+        s.sendall(serialized_message)
 
     def calculate_hash(self):
         hasher = hashlib.sha256()
@@ -34,31 +42,32 @@ class FileSender:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.host, self.port))
 
-            # Send file info
-            file_info = FileShare(
-                datatype=FileShare.DataType.FILE_INFO,
-                fileName=self.file_name,
-                fileSize=self.file_size,
-                hash=self.file_hash,
-            )
-            s.sendall(file_info.SerializeToString())
+            fileshare = pb.FileShare()
+            fileshare.datatype = pb.FileShare.FILE_INFO
+            fileshare.fileName = self.file_name
+            fileshare.fileSize = self.file_size
+            fileshare.hash = self.file_hash
+
+            print(f"Sending FILE_INFO: {self.file_name}, {self.file_size}, {self.file_hash}")  # Debug
+            self.send_message(s, fileshare)
 
             # Send file chunks
             for index, chunk in self.divide_file_into_chunks():
-                piece_message = FileShare(
-                    datatype=FileShare.DataType.PIECE,
-                    pieceIndex=index,
-                    pieceData=chunk,
-                )
-                s.sendall(piece_message.SerializeToString())
+                print(f"Sending chunk {index}, Size: {len(chunk)}")  # Debug
+                piece_message = pb.FileShare()
+                piece_message.datatype = pb.FileShare.PIECE
+                piece_message.pieceIndex = index
+                piece_message.pieceData = chunk
+
+                self.send_message(s, piece_message)
 
             # Send finish signal
-            finish_message = FileShare(
-                datatype=FileShare.DataType.FINISHED
-            )
-            s.sendall(finish_message.SerializeToString())
+            finish_message = pb.FileShare()
+            finish_message.datatype = pb.FileShare.FINISHED
+
+            self.send_message(s, finish_message)
 
 
 if __name__ == "__main__":
-    sender = FileSender("/home/miw/Documents/Python/jokinojo/python/chat/requirements.txt", "127.0.0.1", 5000)
+    sender = FileSender("/home/miw/Videos/2024-11-07 09-35-56.mp4", "127.0.0.1", 5000)
     sender.send_file()
