e58b80b25fb4d0da4bfe6e3cf99dcac8f6f3df3d
emin <medasdan@gmail.com>
2024-12-20 19:37:51 +0300

    I made a cpp server for the application for testing and fun. It works!

--- desktop/MediaPlayer.cpp
+++ desktop/MediaPlayer.cpp
@@ -32,8 +32,13 @@ bool MediaPlayer::initialize() {
 }
 
 void MediaPlayer::setMediaStatus(bool isPaused, int timePosition) {
-    check_mpv_error(mpv_set_property(mpv, "pause", MPV_FORMAT_FLAG, (void *) &(isPaused)));
-    check_mpv_error(mpv_set_property(mpv, "time-pos", MPV_FORMAT_DOUBLE, &(timePosition)));
+    int pausedFlag{0};
+    if (isPaused) {
+        pausedFlag = 1;
+    }
+    check_mpv_error(mpv_set_property(mpv, "pause", MPV_FORMAT_FLAG, (void *) &(pausedFlag)));
+    double timePosSeconds = timePosition/1000.0;
+    check_mpv_error(mpv_set_property(mpv, "time-pos", MPV_FORMAT_DOUBLE, &(timePosSeconds)));
 }
 
 void MediaPlayer::setMediaPausedStatus(bool isPaused) {
@@ -41,9 +46,16 @@ void MediaPlayer::setMediaPausedStatus(bool isPaused) {
 }
 
 std::string MediaPlayer::getFileName() {
-    std::string filename{};
-    check_mpv_error(mpv_get_property(mpv, "filename/no-ext", MPV_FORMAT_STRING, &filename));
-    return filename;
+    const char* filename = nullptr; // Pointer to hold the filename string
+    int error = mpv_get_property(mpv, "filename/no-ext", MPV_FORMAT_STRING, &filename);
+    if (error < 0 || filename == nullptr) { // Handle errors
+        check_mpv_error(error); // Log the error or handle it as needed
+        return {};
+    }
+
+    std::string result(filename); // Convert to std::string
+    mpv_free(const_cast<char*>(filename)); // Free the memory allocated by libmpv
+    return result;
 }
 
 double MediaPlayer::getTimePositionSeconds() {
@@ -59,29 +71,34 @@ int MediaPlayer::getTimePositionMiliseconds() {
 bool MediaPlayer::getIsPaused() {
     int isPaused{};
     mpv_get_property(mpv, "pause", MPV_FORMAT_FLAG, &isPaused);
-    if (isPaused == 0) {
-        m_paused = false;
-    } else if (isPaused == 1) {
-        m_paused = true;
-    }
-    return m_paused;
+    return isPaused == 1;
 }
 
 void MediaPlayer::handleMediaActions() {
     while (true) {
         mpv_event *event = mpv_wait_event(mpv, 0);
         if (isHost()) {
-            bool oldIsPaused = m_paused;;
-            if (oldIsPaused != getIsPaused() || event->event_id == MPV_EVENT_SEEK) { // this get also sets the m_paused property. I will change it to look more stateless.
+            bool oldIsPaused = m_paused;              // Capture the current paused state.
+            bool currentPaused = getIsPaused();
+
+            if (oldIsPaused != currentPaused || event->event_id == MPV_EVENT_SEEK) { // this get also sets the m_paused property. I will change it to look more stateless.
+                m_paused = currentPaused;
                 int timePosData = getTimePositionMiliseconds();
-                bool pausedData = getIsPaused();
+                bool pausedData = currentPaused;
+                std::cout << "is Paused?: " << pausedData << "\n";
                 m_networker.sendMediaStatus(timePosData, pausedData);
             } else if (event->event_id == MPV_EVENT_FILE_LOADED) {
+                std::cout << "host\n";
+                std::cout << "file loaded\n";
                 std::string fileName = getFileName();
+                std::cout << "file name:" << fileName;
                 m_networker.sendFileName(fileName);
             }
         } else { // is not host(client)
+
             if (event->event_id == MPV_EVENT_FILE_LOADED) {
+                std::cout << "not host\n";
+
                 m_networker.sendReadyStatus();
             }
         }
